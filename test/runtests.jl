d3ee147dd0c640ca09b9db7a2b402025d8fe3c8c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-08 01:10:44 -0400

    Create runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,29 @@
 using Base.Test
 using LOCF
 
+novalue = NaN
 a = [1.0, 2.0, 3.0]
-b = [1.0, NaN, 3.0]
-c = [1.0, 2.0, NaN]
-d = [NaN, 2.0, 3.0]
+b = [1.0, novalue, 3.0]
+c = [1.0, 2.0, novalue]
+d = [novalue, 2.0, 3.0]
 
 @test locf(a) == a
 @test locf(b) == [1.0, 1.0, 3.0]
 @test locf(c) == [1.0, 2.0, 2.0]
 @test locf(d) == [2.0, 2.0, 3.0]
 @test locf(d, true) == [2.0, 2.0, 3.0]
-@test locf(d, false) == [NaN, 2.0, 3.0]
+@test isnan(locf(d, false)[1])
+
+novalue = Nullable{Float64}()
+a = [Nullable{Float64}(2.0), Nullable{Float64}(2.0), Nullable{Float64}(2.0)]
+b = [Nullable{Float64}(1.0), novalue, Nullable{Float64}(3.0)]
+c = [Nullable{Float64}(1.0), Nullable{Float64}(2.0), novalue]
+d = [novalue, Nullable{Float64}(2.0), Nullable{Float64}(3.0)]
+
+@test locf(a) == a
+@test locf(b) == [Nullable{Float64}(1.0), Nullable{Float64}(1.0), Nullable{Float64}(3.0)]
+@test locf(c) == [Nullable{Float64}(1.0), Nullable{Float64}(2.0), Nullable{Float64}(2.0)]
+@test locf(d) == [Nullable{Float64}(2.0), Nullable{Float64}(2.0), Nullable{Float64}(3.0)]
+@test locf(d, true) == [Nullable{Float64}(2.0), Nullable{Float64}(2.0), Nullable{Float64}(3.0)]
+@test locf(d, false)[1] == novalue
+
