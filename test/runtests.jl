2bb8b86a5bc8f519261ded8569a23586aab7c953
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-08 00:40:37 -0400

    Create runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,6 @@ d = [NaN, 2.0, 3.0]
 @test locf(a) == a
 @test locf(b) == [1.0, 1.0, 3.0]
 @test locf(c) == [1.0, 2.0, 2.0]
-@test locf(d) == [2.0, 2.0, 2.0]
+@test locf(d) == [2.0, 2.0, 3.0]
 @test locf(d, true) == [2.0, 2.0, 3.0]
 @test locf(d, false) == [NaN, 2.0, 3.0]
