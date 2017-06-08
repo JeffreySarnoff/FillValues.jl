42db33bc59d6820dd7cc544fae81afe7f1704356
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-08 02:03:13 -0400

    Create locf_nulls.jl

--- src/locf_nulls.jl
+++ src/locf_nulls.jl
@@ -30,11 +30,15 @@ function locf!(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:
 
     if isnull(vec[1])
        idx = index_first_nonnull(view(vec, vecidxs))
-       vec[1:idx-1] = vec[idx]
+       if idx != 0
+           vec[1:idx-1] = vec[idx]
+       end
     end
     if isnull(vec[end])
        idx = index_final_nonnull(view(vec, vecidxs))
-       vec[idx+1:end] = vec[idx]
+       if idx != 0
+           vec[idx+1:end] = vec[idx]
+       end     
     end
 
    if any(isnull.(vec))
