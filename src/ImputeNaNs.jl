df5351093c3d4dc7f7daddbbd2ef5d48525fb30c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-14 09:52:24 +0100

    Update ImputeNaNs.jl

--- src/ImputeNaNs.jl
+++ src/ImputeNaNs.jl
@@ -4,6 +4,7 @@ export locf, locf!
 
 const AbstractIntVec = AbstractVector{T} where T<:Integer
 const AbstractFloatVec = AbstractVector{T} where T<:AbstractFloat
+const NullableNumVec = AbstractVector{N} where N<:Nullable{T} where T
 
 include("locf_nans.jl")
 include("locf_nulls.jl")
