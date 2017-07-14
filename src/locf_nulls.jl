b2db2a9759fb27b139e3ee78f4492c323f5431eb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-07-14 09:42:42 +0100

    Update locf_nulls.jl

--- src/locf_nulls.jl
+++ src/locf_nulls.jl
@@ -1,4 +1,4 @@
-function locf(vec::V, fillback::Bool) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function locf(vec::NullableNumVec, fillback::Bool)
    idx = index_first_nonnull(vec)
    v = locf(vec)
    if !fillback && idx > 1
@@ -7,13 +7,13 @@ function locf(vec::V, fillback::Bool) where V<:AbstractVector{N} where N<:Nullab
    return v
 end
 
-function locf(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function locf(vec::NullableNumVec)
    v = copy(vec)
    locf!(v)
    return v
 end
 
-function locf!(vec::V, fillback::Bool) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function locf!(vec::NullableNumVec, fillback::Bool)
    idx = index_first_nonnull(vec)
    if !fillback && idx > 1
       vecstart = vec[1:idx-1]
@@ -25,7 +25,7 @@ function locf!(vec::V, fillback::Bool) where V<:AbstractVector{N} where N<:Nulla
    return nothing
 end
 
-function locf!(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function locf!(vec::NullableNumVec)
     n = length(vec)
     vecidxs = 1:n
 
@@ -53,7 +53,7 @@ function locf!(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:
     return nothing
 end
 
-function locf_values(vec::V, nulls_at::I) where I<:AbstractVector{Int} where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function locf_values(vec::NullableNumVec, nulls_at::AbstractIntVec)
     augment = 0
     deltas = [nulls_at[1]-1, diff(nulls_at)...]
 
@@ -70,7 +70,7 @@ function locf_values(vec::V, nulls_at::I) where I<:AbstractVector{Int} where V<:
     return cumsum(deltas)
 end
 
-function index_nulls(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function index_nulls(vec::NullableNumVec)
     idxs = 1:length(vec)
     nulls = map(isnull, view(vec,idxs))
     return idxs[nulls]
@@ -81,7 +81,7 @@ end
 
 returns 0 iff all elements of vec are Null
 """
-function index_first_nonnull(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function index_first_nonnull(vec::NullableNumVec)
    result = 0
    for i in 1:length(vec)
       if !isnull(vec[i])
@@ -97,7 +97,7 @@ end
 
 returns 0 iff all elements of vec are Null
 """
-function index_final_nonnull(vec::V) where V<:AbstractVector{N} where N<:Nullable{T} where T<:Number
+function index_final_nonnull(vec::NullableNumVec)
    result = 0
    for i in length(vec):-1:1
       if !isnull(vec[i])
