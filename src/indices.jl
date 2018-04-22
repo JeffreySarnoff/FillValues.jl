6146b0f7f882159e69d17a1dbea779b9b1c0b77e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-22 09:07:13 -0400

    Update indices.jl

--- src/indices.jl
+++ src/indices.jl
@@ -3,3 +3,14 @@
 findmissings(x::AbstractArray{T}) where {T} = findall(x .=== missing)
 findpresents(x::AbstractArray{T}) where {T} = findall(x .!== missing)
 
+for T in (:Int8, :Int16, :Int32, :Int64, :Int128,
+          :UInt8, :UInt16, :UInt32, :UInt64, :UInt128,
+          :Float16, :Float32, :Float64, :String, :Char)
+  @eval begin
+     removemissings(x::AbstractArray{$T}) = x
+     function removemissings(x::AbstractArray{Union{Missing,$T}})
+        result::$T = filter(!ismissing, x)
+        return result
+     end
+  end
+end
