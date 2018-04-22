d52b68226f5b89bf2f1cfe4054d45e14b0efabe4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-22 09:08:36 -0400

    Update indices.jl

--- src/indices.jl
+++ src/indices.jl
@@ -1,14 +1,14 @@
 # determine if any of an ordered sequence are missing
 
-findmissings(x::AbstractArray{T}) where {T} = findall(x .=== missing)
-findpresents(x::AbstractArray{T}) where {T} = findall(x .!== missing)
+findmissings(x::AbstractArray{T,N}) where {N,T} = findall(x .=== missing)
+findpresents(x::AbstractArray{T,N}) where {N,T} = findall(x .!== missing)
 
 for T in (:Int8, :Int16, :Int32, :Int64, :Int128,
           :UInt8, :UInt16, :UInt32, :UInt64, :UInt128,
           :Float16, :Float32, :Float64, :String, :Char)
   @eval begin
-     removemissings(x::AbstractArray{$T}) = x
-     function removemissings(x::AbstractArray{Union{Missing,$T}})
+     removemissings(x::AbstractArray{$T,N}) where {N} = x
+     function removemissings(x::AbstractArray{Union{Missing,$T},N}) where {N}
         result::$T = filter(!ismissing, x)
         return result
      end
