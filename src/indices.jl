620041c7a84c2d8610b74d47b3c964ae7648805d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-04-22 08:25:39 -0400

    Update indices.jl

--- src/indices.jl
+++ src/indices.jl
@@ -1,1 +1,5 @@
+# determine if any of an ordered sequence are missing
+
+findmissings(x::AbstractArray{T}) where {T} = findall(x .=== missing)
+findpresents(x::AbstractArray{T}) where {T} = findall(x .!== missing)
 
